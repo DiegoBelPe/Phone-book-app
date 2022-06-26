aa83cd78a574b50b20e8eeace9b247b0a84641e8
DiegoBelPe <dbeltranpereira@gmail.com>
2022-06-26 14:18:02 -0500

    routes app ok

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Prueba from './components/Header';
-import NewContact from './components/NewContact';
+import Layout from './layout/Layout';
+import Inicio from './pages/Inicio';
+import NewContact from './pages/NewContact' 
+import EditContact from './pages/EditContact'; 
+
 
 function App() {
   return (
     <BrowserRouter>
-     <Routes>
-     <Route path="/" element={<Prueba />} />
-     <Route path="/contacto/nuevo" element={<NewContact />} />
-     </Routes>
-    
-    </BrowserRouter>
+          <Routes>
+              <Route path="/" element={<Layout />}>
+                  <Route index element={<Inicio />} />
+                  <Route path="contactos/nuevo" element={<NewContact />} />
+                  <Route path="contactos/editar/:id" element={<EditContact />} />
+                  {/* <Route path=":id" element={<VerCliente />} /> */}
+              </Route>
+          </Routes>
+      </BrowserRouter>
   );
 }
 
