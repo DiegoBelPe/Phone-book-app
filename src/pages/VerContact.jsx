c8a59d5a06443caf17a4598242de2989b87a0b92
DiegoBelPe <dbeltranpereira@gmail.com>
2022-06-26 22:24:17 -0500

    api-call method get id contact-spinner

--- src/pages/VerContact.jsx
+++ src/pages/VerContact.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState} from 'react'
 import { useParams } from 'react-router-dom'
 import { getContact } from '../services/contact'
+import Spinner from '../components/Spinner'
 
 const VerContact = () => {
   const { id } = useParams()
   const [contact, setContact] = useState({})
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const fetchData = async () => {
@@ -14,29 +16,38 @@ const VerContact = () => {
       } catch (error) {
         console.log(error)
       }
+      setTimeout(() => {
+        setLoading(!loading)
+      }, 500)
+      
     }
     fetchData()
   }, [])
   return (
-    <div>
-      <h1 className="font-black text-4xl text-blue-900">Ver Contacto: {contact.nombre}</h1>
-      <p className="mt-3">Informacion Contacto</p>
-      <p className='text-3xl text-gray-600 '>
-        <span className='font-bold text-gray-700 uppercase'>Nombre:</span> {contact.nombre}
-      </p>
-      <p className='text-2xl text-gray-600 mt-4 '>
-        <span className='font-bold text-gray-700 uppercase'>Telefono:</span> {contact.telefono}
-      </p>
-      <p className='text-2xl text-gray-600 mt-4 '>
-        <span className='font-bold text-gray-700 uppercase'>Edad:</span> {contact.fechaNacimiento}
-      </p>
-      <p className='text-2xl text-gray-600 mt-4 '>
-        <span className='font-bold text-gray-700 uppercase'>Direccion:</span> {contact.direccion}
-      </p>
-      <p className='text-2xl text-gray-600 mt-4 '>
-        <span className='font-bold text-gray-700 uppercase'>Email:</span> {contact.email}
-      </p>
-    </div>
+    loading ? <Spinner /> : 
+      Object.keys(contact).length === 0 ? 
+      <p> No hay resultados</p> : 
+     (
+      <div>
+          <h1 className="font-black text-4xl text-blue-900">Ver Contacto: {contact.nombre}</h1>
+          <p className="mt-3">Informacion Contacto</p>
+          <p className='text-3xl text-gray-600 '>
+            <span className='font-bold text-gray-700 uppercase'>Nombre:</span> {contact.nombre}
+          </p>
+          <p className='text-2xl text-gray-600 mt-4 '>
+            <span className='font-bold text-gray-700 uppercase'>Telefono:</span> {contact.telefono}
+          </p>
+          <p className='text-2xl text-gray-600 mt-4 '>
+            <span className='font-bold text-gray-700 uppercase'>Edad:</span> {contact.fechaNacimiento}
+          </p>
+          <p className='text-2xl text-gray-600 mt-4 '>
+            <span className='font-bold text-gray-700 uppercase'>Direccion:</span> {contact.direccion}
+          </p>
+          <p className='text-2xl text-gray-600 mt-4 '>
+            <span className='font-bold text-gray-700 uppercase'>Email:</span> {contact.email}
+          </p>
+      </div>
+    )
   )
 }
 
