9d5dd7d6d65c9041f9dec33fa5fc3a3fd022bde5
DiegoBelPe <dbeltranpereira@gmail.com>
2022-06-27 13:01:45 -0500

    aplication phone end ok

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -5,6 +5,23 @@ import Contacto from '../components/Contacto'
 const Inicio = () => {
   // eslint-disable-next-line no-unused-vars
   const [contacts, setContacts] = useState([])
+  const [search, setSearch] = useState('')
+
+  const searchContacts = (e) => {
+    setSearch(e.target.value)
+  }
+
+  //metodo de filtrado de contactos
+  let results = []
+  if(!search){
+    results = contacts
+  }else{
+    results = contacts.filter(contact => {
+      return contact.nombre.toLowerCase().includes(search.toLowerCase()),
+      contact.email.toLowerCase().includes(search.toLowerCase())
+    }
+    )
+  }
 
   useEffect(() => {
     const fetchData = async () => {
@@ -36,6 +53,14 @@ const Inicio = () => {
       <h1 className="font-black text-4xl text-blue-900">Contactos</h1>
       <p className="mt-3">Administra tus contactos</p>
 
+      <input 
+      type="text"
+      className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+      placeholder="Buscar contacto"
+      value={search}
+      onChange={searchContacts}
+      ></input>
+
       <table className='w-full mt-5 table-auto shadow bg-white'>
         <thead className='bg-blue-800 text-white'>
           <tr>
@@ -48,7 +73,7 @@ const Inicio = () => {
 
         </thead>
         <tbody>
-          {contacts.map(contact => (
+          {results.map(contact => (
             <Contacto
                key={contact.id} 
                contacto={contact}
